46960df2e9786a5ab1bc53b8cfd17df1e8b36fcd
Kenichi Koyama <pianist.koya@gmail.com>
2015-03-31 03:14:07 +0900

    バンオフ新規作成画面を追加

---
+++ client/app/scripts/controllers/new_event.js
@@ -0,0 +1,64 @@
+(function () {
+  'use strict';
+
+  angular.module('clientApp')
+    .controller('NewEventController', function ($scope, $resource, $state, globalAlert) {
+      $scope.minDate = new Date();
+      $scope.startTime = new Date($scope.minDate.getFullYear(), $scope.minDate.getMonth(), $scope.minDate.getDate(), 11, 0, 0);
+      $scope.endTime = new Date($scope.minDate.getFullYear(), $scope.minDate.getMonth(), $scope.minDate.getDate(), 18, 0, 0);
+      $scope.eventName = '';
+      $scope.scheduledDate = null;
+
+      $scope.alert = globalAlert.getAndClear();
+
+      $scope.submit = function() {
+
+        var Event = $resource('http://localhost:8081/event');
+
+        $scope.startTime = new Date($scope.scheduledDate.getFullYear(), $scope.scheduledDate.getMonth(), $scope.scheduledDate.getDate(),
+          $scope.startTime.getHours(), $scope.startTime.getMinutes(), 0);
+        $scope.endTime = new Date($scope.scheduledDate.getFullYear(), $scope.scheduledDate.getMonth(), $scope.scheduledDate.getDate(),
+          $scope.endTime.getHours(), $scope.endTime.getMinutes(), 0);
+
+        var event = new Event({
+          name: $scope.eventName,
+          startTime: $scope.startTime,
+          endTime: $scope.endTime
+        });
+
+        event.$save(
+          function() {
+            globalAlert.set({type: 'success', msg: 'バンオフを作成しました！'});
+            $state.go('portal');
+          },
+          function() {
+            $scope.alert = {type: 'danger', msg: 'おや、失敗しました'};
+          }
+        );
+      };
+
+      $scope.today = function() {
+        $scope.scheduledDate = new Date();
+      };
+
+      $scope.clear = function () {
+        $scope.scheduledDate = null;
+      };
+
+      $scope.open = function($event) {
+        $event.preventDefault();
+        $event.stopPropagation();
+
+        $scope.opened = true;
+      };
+
+      $scope.dateOptions = {
+        formatYear: 'yy',
+        startingDay: 1,
+        showWeeks: false
+      };
+
+
+    });
+
+})();
