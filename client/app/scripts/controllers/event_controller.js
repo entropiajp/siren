39b1e40284d117a6fe7a510a904037d65d4a5346
Kenichi Koyama <pianist.koya@gmail.com>
2015-03-31 03:17:30 +0900

    バンオフ情報画面を追加

---
+++ client/app/scripts/controllers/event_controller.js
@@ -0,0 +1,46 @@
+(function () {
+  'use strict';
+
+  angular.module('clientApp')
+    .controller('EventController', function ($scope, globalAlert, $stateParams, Event, SweetAlert, Member) {
+
+      $scope.alert = globalAlert.getAndClear();
+      $scope.event = null;
+
+      Event.find($stateParams.eventId).then(
+        function(data){
+          data.startTime = new Date(data.startTime);
+          data.endTime = new Date(data.endTime);
+          $scope.event = data;
+        });
+
+      $scope.join = function() {
+        SweetAlert.swal({
+            title: "このバンオフに参加しますか？",
+            text: "参加キャンセルする場合、主催まで連絡してください",
+            type: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#DD6B55",
+            confirmButtonText: "参加する！",
+            closeOnConfirm: true
+          },
+          function (isConfirm) {
+            if(isConfirm) {
+              Member.join($stateParams.eventId).then(
+                function () {
+                  $scope.alert = {type: 'success', msg: 'バンオフに参加しました！'};
+                },
+                function () {
+                  $scope.alert = {type: 'warning', msg: 'おや、失敗しました'};
+                }
+              );
+            }
+          }
+        );
+      }
+
+
+    });
+
+
+})();
