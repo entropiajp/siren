53f98190e4e014fabba31e5ee644daa1e586853e
Kenichi Koyama <pianist.koya@gmail.com>
2015-03-31 02:57:32 +0900

    [client]index.htmlスクリプト呼び出し追加。app.'sでstateの定義など追加。css修正

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -1,34 +1,84 @@
-'use strict';
+(function(){
+  'use strict';
 
-/**
- * @ngdoc overview
- * @name clientApp
- * @description
- * # clientApp
- *
- * Main module of the application.
- */
-angular
-  .module('clientApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch',
-    'ui.bootstrap'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+  angular
+    .module('clientApp', [
+      'ngAnimate',
+      'ngCookies',
+      'ngResource',
+      'ngSanitize',
+      'ngTouch',
+      'ui.bootstrap',
+      'ui.router',
+      'oitozero.ngSweetAlert',
+      'angular-loading-bar'
+    ])
+    .config(function ($stateProvider, $httpProvider, $urlRouterProvider) {
+      $stateProvider
+        .state("new", {
+          url: "/event/new",
+          templateUrl: "views/new_event.html",
+          controller: "NewEventController"
+        })
+        .state("portal", {
+          url: "/portal",
+          templateUrl: "views/portal.html",
+          controller: "PortalController"
+        })
+        .state("login", {
+          url: "/login",
+          templateUrl: "views/login.html",
+          controller: "LoginController"
+        })
+        .state("event", {
+          url: "/event/:eventId",
+          templateUrl: "views/event.html",
+          controller: "EventController"
+        })
+        .state("edit_event", {
+          url: "/event/:eventId/edit",
+          templateUrl: "views/edit_event.html",
+          controller: "EditEventController"
+        })
+        .state("edit_candidate", {
+          url: "/event/:eventId/candidate/edit",
+          templateUrl: "views/edit_candidate.html",
+          controller: "CandidateController"
+        })
+        .state("edit_members", {
+          url: "/event/:eventId/member/edit",
+          templateUrl: "views/edit_members.html",
+          controller: "MemberController"
+        })
+        .state("new_tune", {
+          url: "/tune/new",
+          templateUrl: "views/new_tune.html",
+          controller: "NewTuneController"
+        })
+        .state("help", {
+          url: "/help",
+          templateUrl: "views/help.html"
+        });
+
+      $urlRouterProvider.otherwise('/portal');
+
+      // サーバ側でajax通信を判定するためのheader追加
+      $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
+
+      // CookieのセッションIDをすべてのリクエストに付与するための設定
+      $httpProvider.defaults.withCredentials = true;
+
+      // ログインしていなければログインページヘ強制遷移させるinterceptor
+      $httpProvider.interceptors.push(function ($q, $rootScope, $location) {
+          return {
+            'responseError': function(response) {
+              if (response.status === 401) {
+                $location.path('/login');
+              }
+              return $q.reject(response);
+            }
+          };
+        }
+      );
+    });
+})();
