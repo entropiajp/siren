7e2687129f2b7910e639855f94e8cab35d4de0fb
Kenichi Koyama <pianist.koya@gmail.com>
2015-05-23 02:19:45 +0900

    バンオフ概要画面に本番セットリストの情報を表示するように変更

--- client/app/scripts/controllers/event_controller.js
+++ client/app/scripts/controllers/event_controller.js
@@ -2,7 +2,7 @@
   'use strict';
 
   angular.module('clientApp')
-    .controller('EventController', function ($scope, globalAlert, $stateParams, SweetAlert, Member, UtilService, Part, $modal, $state, event) {
+    .controller('EventController', function ($scope, globalAlert, $stateParams, SweetAlert, Member, UtilService, Part, $modal, $state, event, Song) {
 
       $scope.alert = globalAlert.getAndClear();
       $scope.event = event;
@@ -10,6 +10,7 @@
       $scope.isVotingPeriod = UtilService.isBetween($scope.event.voteStartTime, $scope.event.voteEndTime);
       $scope.managers = null;
       $scope.members = null;
+      $scope.songs = null;
 
       Member.query($stateParams.eventId).then(
         function(data) {
@@ -24,6 +25,24 @@
             data[i].playable = false;
           }
           $scope.parts = data;
+        }
+      );
+
+      Song.query($stateParams.eventId).then(
+        function(data){
+          data = data
+            .filter(function(e){return e.song.playable})
+            .map(function(item){
+              if(item.roles !== null) {
+                item.roles = item.roles.filter(function(role){return role.userId !== null;});
+              }
+              return item;
+            });
+
+          for(var i=0; i<data.length; i++) {
+            data[i].song.time = UtilService.convertTimeToDateObject(data[i].song.time, new Date());
+          }
+          $scope.songs = data;
         }
       );
 
